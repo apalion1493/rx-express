fb87eb73fd85455aa53b2fcb130554d22a5082fa
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-02-23 09:27:48 +0200

    test2

--- app/js/main.js
+++ app/js/main.js
@@ -21,7 +21,10 @@ $(window).width(function() {
     if(document.documentElement.clientWidth < 767.98) {
         $(".rx-express-header__main-imgHuman-blockLeft img").remove();
         $(".rx-express-header__main-imgHuman-blockRight img").remove();
+        $(".rx-express-header__main-comments img").remove();
         $('.rx-express-header__main-imgHuman-blockLeft').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/hipster-mobil.png" alt="">');
         $('.rx-express-header__main-imgHuman-blockRight').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/banker-mobil.png" alt="">');
+        $('.rx-express-header__main-comments').append('<img class="rx-express-header__main-comments-left" src="img/left-comment__mobil.png" alt="">');
+        $('.rx-express-header__main-comments').append('<img class="rx-express-header__main-comments-left" src="img/right-comment__mobil.png" alt="">');
     }
 });
