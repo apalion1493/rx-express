625e6efaf18a0829f00c2ec49df03d66453d67e1
apalion1493 <39518411+apalion1493@users.noreply.github.com>
2019-02-25 07:53:09 +0200

    test7

--- app/js/main.js
+++ app/js/main.js
@@ -22,9 +22,77 @@ $(window).width(function() {
         $(".rx-express-header__main-imgHuman-blockLeft img").remove();
         $(".rx-express-header__main-imgHuman-blockRight img").remove();
         $(".rx-express-header__main-comments img").remove();
-        $('.rx-express-header__main-imgHuman-blockLeft').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/hipster-mobil.png" alt="">');
-        $('.rx-express-header__main-imgHuman-blockRight').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/banker-mobil.png" alt="">');
+        $('.rx-express-header__main-imgHuman-blockLeft.rx-express-header__main-imgHuman-basis').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/hipster-mobil.png" alt="">');
+        $('.rx-express-header__main-imgHuman-blockRight.rx-express-header__main-imgHuman-basis').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/banker-mobil.png" alt="">');
+
+        $('.rx-express-header__main-imgHuman-blockLeft.rx-express-header__main-imgHuman-blockLeft-alternative').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/man-mob.png" alt="">');
+        $('.rx-express-header__main-imgHuman-blockRight.rx-express-header__main-imgHuman-blockRight-alternative').append('<img class="rx-express-header__main-imgHuman-img rx-express-header__main-imgHuman-left" src="img/woman-mob.png" alt="">');
+
         $('.rx-express-header__main-comments').append('<img class="rx-express-header__main-comments-left" src="img/left-comment__mobil.png" alt="">');
         $('.rx-express-header__main-comments').append('<img class="rx-express-header__main-comments-right" src="img/right-comment__mobil.png" alt="">');
     }
 });
+
+
+//accordion
+//uses classList, setAttribute, and querySelectorAll
+//if you want this to work in IE8/9 youll need to polyfill these
+(function(){
+    var d = document,
+        accordionToggles = d.querySelectorAll('.js-accordionTrigger'),
+        setAria,
+        setAccordionAria,
+        switchAccordion,
+        touchSupported = ('ontouchstart' in window),
+        pointerSupported = ('pointerdown' in window);
+
+    skipClickDelay = function(e){
+        e.preventDefault();
+        e.target.click();
+    };
+
+    setAriaAttr = function(el, ariaType, newProperty){
+        el.setAttribute(ariaType, newProperty);
+    };
+    setAccordionAria = function(el1, el2, expanded){
+        switch(expanded) {
+            case "true":
+                setAriaAttr(el1, 'aria-expanded', 'true');
+                setAriaAttr(el2, 'aria-hidden', 'false');
+                break;
+            case "false":
+                setAriaAttr(el1, 'aria-expanded', 'false');
+                setAriaAttr(el2, 'aria-hidden', 'true');
+                break;
+            default:
+                break;
+        }
+    };
+//function
+    switchAccordion = function(e) {
+        console.log("triggered");
+        e.preventDefault();
+        var thisAnswer = e.target.parentNode.nextElementSibling;
+        var thisQuestion = e.target;
+        if(thisAnswer.classList.contains('is-collapsed')) {
+            setAccordionAria(thisQuestion, thisAnswer, 'true');
+        } else {
+            setAccordionAria(thisQuestion, thisAnswer, 'false');
+        }
+        thisQuestion.classList.toggle('is-collapsed');
+        thisQuestion.classList.toggle('is-expanded');
+        thisAnswer.classList.toggle('is-collapsed');
+        thisAnswer.classList.toggle('is-expanded');
+
+        thisAnswer.classList.toggle('animateIn');
+    };
+    for (var i=0,len=accordionToggles.length; i<len; i++) {
+        if(touchSupported) {
+            accordionToggles[i].addEventListener('touchstart', skipClickDelay, false);
+        }
+        if(pointerSupported){
+            accordionToggles[i].addEventListener('pointerdown', skipClickDelay, false);
+        }
+        accordionToggles[i].addEventListener('click', switchAccordion, false);
+    }
+})();
